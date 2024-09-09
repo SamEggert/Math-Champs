dac3b6fbf8cd9f49592f022e5f7744654b058aa1
SamEggert <137643774+SamEggert@users.noreply.github.com>
2024-09-09 10:47:30 -0400

    Initial Commit

---
+++ Math Champs/Math_ChampsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_ChampsApp.swift
+//  Math Champs
+//
+//  Created by Samuel Eggert on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Math_ChampsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
